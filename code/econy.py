2d2fc69f6f933d6e4c74eb6a10a112fb337622d9
李宜,LI YI <s411178021@gm.ntpu.edu.tw>
2024-11-01 19:37:06 +0800

    Update econy.py

--- code/econy.py
+++ code/econy.py
@@ -61,7 +61,7 @@ def scrape_flights(start_date_str, end_date_str):
     while current_date <= end_date:
         print(f"正在抓取日期: {current_date.strftime('%Y-%m-%d')}")
 
-        url = "https://www.google.com/travel/flights/search?tfs=CBwQAholEgoyMDI1LTAxLTE3KAFqDAgCEggvbS8wZnRreHIHCAESA0pGS0ABSAFwAYIBCwj___________wGYAQI&hl=zh-TW&gl=TW000000000000000000000000000/"
+        url = "https://www.google.com/travel/flights/search?tfs=CBwQAholEgoyMDI1LTAxLTE5KAFqDAgCEggvbS8wZnRreHIHCAESA0pGS0ABSAFwAYIBCwj___________8BmAEC&tfu=EgYIABABGAA&hl=zh-TW&gl=TW"
         driver.get(url)
 
         # 點擊日期選擇器
