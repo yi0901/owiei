37a01635dd22ba018f06ae009e2211dd58662dd6
李宜,LI YI <s411178021@gm.ntpu.edu.tw>
2024-12-03 12:53:29 +0800

    Update ecolondon4.py

--- code/ecolondon4.py
+++ code/ecolondon4.py
@@ -69,8 +69,8 @@ def click_element(element):
     except Exception as e:
         print(f"點擊元素失敗: {e}")
         return False
-start_date = "2025-03-03"
-end_date = "2025-03-03"
+start_date = "2025-03-01"
+end_date = "2025-03-04"
 def scrape_flights(start_date_str, end_date_str):
     start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
     end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
