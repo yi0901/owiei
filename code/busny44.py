716a1fea37143d89dd4236dbccefcae35df3758e
李宜,LI YI <s411178021@gm.ntpu.edu.tw>
2024-12-14 11:36:54 +0800

    Update and rename busny3.py to busny44.py

--- code/busny44.py
+++ code/busny44.py
@@ -70,7 +70,7 @@ def click_element(element):
         print(f"點擊元素失敗: {e}")
         return False
 start_date = "2025-03-02"
-end_date = "2025-03-14"
+end_date = "2025-03-15"
 def scrape_flights(start_date_str, end_date_str):
     start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
     end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
@@ -152,7 +152,7 @@ def scrape_flights(start_date_str, end_date_str):
             os.makedirs(output_directory)
         
         # 準備寫入 CSV 檔案
-        with open(f'{output_directory}/busny3_{today_date}.csv', 'a', newline='', encoding='utf-8-sig') as csv_file:
+        with open(f'{output_directory}/busny4_{today_date}.csv', 'a', newline='', encoding='utf-8-sig') as csv_file:
             csv_writer = csv.writer(csv_file)
 
             # 寫入標題
